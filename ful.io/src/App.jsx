c8c934667891fcfd27006de1ba4c684095e0cae4
PrabhuP1 <prabhupugal01@gmail.com>
2023-02-25 13:16:40 +0530

    commit1

---
+++ ful.io/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <span className="text-black">Hello</span>
+    </div>
+  );
+}
+
+export default App;
